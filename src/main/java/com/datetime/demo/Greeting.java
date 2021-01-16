45c019c59b36e777b4172cca6df09eba3cb8e7bc
Dan Bitter <HireDanBitter@gmail.com>
2021-01-16 16:16:36 -0600

    final was causing error "No Creators, like default construct, exist): cannot deserialize from Object value (no delegate- or property-based Creator"

--- src/main/java/com/datetime/demo/Greeting.java
+++ src/main/java/com/datetime/demo/Greeting.java
@@ -4,14 +4,15 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 
 //@RequiredArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Greeting {
 
 //    @JsonProperty("isSet") for when json variable != java variable
-    private final long id;
-    private final @NonNull String content;
+    private long id;
+    private @NonNull String content;
 
     public Greeting(long id, String content) {
         this.id = id;
