ebc03e9a7ca5db09faf65ba8290eaa265af7999c
Dushyant <dushyant.dubaria2010@gmail.com>
2021-02-05 12:23:50 -0600

    Corrected port

--- DD-notes.sh
+++ DD-notes.sh
@@ -2,9 +2,13 @@
 
 docker build -t datetime:latest --build-arg server_port=8083 --build-arg rest_hostname=greeting --build-arg rest_port=8084 --no-cache .
 docker stop datetime && docker rm datetime 
-docker run -d -p 8083:8083 --name datetime datetime
-curl localhost:8083/currentdatetime
+docker run -d -p 8084:8083 --name datetime datetime
+curl localhost:8084/currentdatetime
 
+
+docker logs datetime
+docker exec -it datetime sh 
+apk add curl 
 
 
 git add .
